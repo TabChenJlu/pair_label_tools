edd9d1f6378aea7ab48b10088652e680b91a9c4f
TabChen <jiahaochen@100tal.com>
2019-03-12 22:22:32 +0800

    remove not use backage

--- start.py
+++ start.py
@@ -2,7 +2,6 @@ import dash
 import os
 import configparser
 import dash_core_components as dcc
-import dash_bootstrap_components as dbc
 import dash_html_components as html
 from dash.dependencies import Input, Output, State
 from tools.utils import *
